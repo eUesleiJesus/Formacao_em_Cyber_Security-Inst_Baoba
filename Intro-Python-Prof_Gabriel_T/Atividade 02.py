9d612d35eebbc79974ea9197b9024d4f4a9bed8a
eUesleiJesus <uesleijesuspsi@hotmail.com>
2022-05-31 20:54:33 -0700

    Atividade 01

---
+++ Intro-Python-Prof_Gabriel_T/Atividade 02.py
@@ -0,0 +1,2 @@
+x = int(input("Digite um numero: "))
+print(x)
---
+++ Intro-Python-Prof_Gabriel_T/Atividade 03.py
@@ -0,0 +1,15 @@
+import mean on statistics
+
+list =  [1, 2, 3, 4, 5, 6]
+mean = statistics.mean(list)
+print(mean)
+
+
+#notas_str = input("Informe as quatros notas: ")
+#notas_int = [int(i) for i in notas_str]
+#nota = 0
+
+##for i in range(4):
+    #nota += notas_int[i]
+
+#(nota/4)
